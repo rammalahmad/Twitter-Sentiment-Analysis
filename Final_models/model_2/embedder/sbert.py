262e79906dd7c5a2fdb43db88ef8aa98858c03b5
HusseinRR <rammal.hussein@outlook.com>
2022-09-06 18:19:42 +0200

    Reorganisation Git

--- Final_models/model_2/embedder/sbert.py
+++
@@ -1,8 +0,0 @@
-'''
-# Info
----
-In this script we just import the sbert model, it's multiling
-'''
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
